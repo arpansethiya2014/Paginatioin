f5ff6249cdc0a13009a0c7413b10eac75cb204af
arpansethiya78699 <arpansethiya2014@gmail.com>
2020-12-13 18:18:54 +0530

    add variables

--- src/main/javacode/com/bean/Employee.java
+++ src/main/javacode/com/bean/Employee.java
@@ -9,12 +9,10 @@ public class Employee {
 	private int salary;
 	private String department;
 	private Timestamp timestamp;
-	
-	
-	
+
 	public Employee() {
 	}
-	
+
 	public Employee(int id, String name, int salary, String department, Timestamp timestamp) {
 		this.id = id;
 		this.name = name;
@@ -22,37 +20,45 @@ public class Employee {
 		this.department = department;
 		this.timestamp = timestamp;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getSalary() {
 		return salary;
 	}
+
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
 	public Timestamp getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(Timestamp timestamp) {
 		this.timestamp = timestamp;
 	}
-	
-	
-	
+
 }
